0f302e7a2bb3571ef9c79498c5513301c50b367a
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-06-12 15:35:20 -0500

    adds npm installs

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+/// Add middleware  ///
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+/// Add the application routes ///
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT ///
+app.listen(PORT, function() {
+  console.log('App is listening on PORT: ' + PORT);
+});
