513068bf528aa12f62fbead92aacd6c4360904c3
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-06-13 11:06:46 -0500

    adds code to get the best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,11 +12,35 @@ module.exports = function (app) {
 /// adding a new friend to the friends array ///
 app.post('/api/friends', function (request, response) {
 
-/// extract user's input scores ///
+    /// extract user's input scores ///
     var userInput = request.body;
     var userScores = userInput.scores;
 
-    
+    /// set an empty array for the scores difference of the new user and other users in the array ///
+    var scoreDiffArr = [];
 
+    /// for each friend the object array, extract the score values & parse them ///
+    friendsData.forEach(obj => {
+        var scoreDiff = 0;
+        obj.scores.forEach((item, index) => {
+            var friendScore = parseInt(item);
+            userScores = parseInt(userScores[index]);
 
-})
+            /// get the score difference by taking the absolute value of the resulting number ///
+            scoreDiff += Math.abs(friendScore - userScores);
+
+            console.log(scoreDiff);
+        });
+
+        /// push the difference between scores (scoreDiff) into the score difference array ///
+        scoreDiffArr.push(scoreDiff);
+    });
+    /// returning the lowest score 
+    var newFriend = friendsData[scoreDiffArr.indexOf(Math.min(...scoreDiffArr))]
+
+    /// push the new data ///
+    friendsData.push(userInput);
+
+    response.json(newFriend);
+
+});
