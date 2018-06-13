e53070642ebe6395d21205865e4a83373b244381
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-06-13 14:02:12 -0500

    changes for each to loop

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,45 +9,41 @@ var app = express();
 var friendsData = require('../data/friends.js');
 
 /// get the user data from the friends array and put in a JSON obj ///
-module.exports = function(app) {
+module.exports = function (app) {
+    app.get("/api/friends", function (request, response) {
+        res.json(friendsData);
 
-    app.get('/api/friends', function (request, response) {
-        response.json(friendsData);
     });
+
+    
+    
+    /// adding a new friend to the friends array ///
+    app.post("/api/friends", function (request, response) {
+
+        var userInput = request.body;
+        var userScores = userInput.scores;
+
+        var matchName = '';
+        var matchImage = '';
+        var friendScoreDiff = 1000;
+
+        for (var i = 0; i < friendsData.length; i++) {
+            var scoreDiff = 0;
+            for (var j = 0; j < userScores.length; j++) {
+                scoreDiff += Math.abs(friendsData[i].scores[j] - userScores[j]);
+                console.log(scoreDiff);
+            }
+            if (scoreDiff < friendScoreDiff) {
+                friendScoreDiff = scoreDiff;
+                matchName = friendsData[i].name;
+                matchImage = friendsData[i].photo;
+            }
+
+            friendsData.push(userData);
+            response.json({ status: 'OK', matchName: matchName, matchImage: matchImage });
+        }
+    });
+
 };
 
-/// adding a new friend to the friends array ///
-app.post('/api/friends', function (request, response) {
 
-    /// extract user's input scores ///
-    var userInput = request.body;
-    var userScores = userInput.scores;
-
-    /// set an empty array for the scores difference of the new user and other users in the array ///
-    var scoreDiffArr = [];
-
-    /// for each friend the object array, extract the score values & parse them ///
-    friendsData.forEach(obj => {
-        var scoreDiff = 0;
-        obj.scores.forEach((item, index) => {
-            var friendScore = parseInt(item);
-            var userScore = parseInt(request.body.scores[index]);
-
-            /// get the score difference by taking the absolute value of the resulting number ///
-            scoreDiff += Math.abs(friendScore - userScore);
-
-            
-        });
-
-        /// push the difference between scores (scoreDiff) into the score difference array ///
-        scoreDiffArr.push(scoreDiff);
-    });
-    /// returning the lowest score 
-    var newFriend = friendsData[scoreDiffArr.indexOf(Math.min(...scoreDiffArr))]
-
-    /// push the new data ///
-    friendsData.push(userInput);
-
-    response.json(newFriend);
-
-});
